65a9c02fe8cb561fafb1fcde83463e21049e6a36
Roshan-ch <rosurrch@gmail.com>
2025-01-12 15:12:33 +0545

    removed unwanted codes

--- Frontend/rocket/tailwind.config.js
+++ Frontend/rocket/tailwind.config.js
@@ -3,16 +3,12 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: 'class', // Enable dark mode with class strategy
   theme: {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
         title: ['Rubik Distressed', 'serif'],
         paragraph: ["Jersey 15", 'serif'],
-      },
-      backgroundColor: {
-        'gray-100': '#f3f4f6',
       },
     },
   },
